0488a7a63bcdca5ba1b41e99e26b8177cad616f9
chulhyun96 <park82602@gmail.com>
2025-09-07 18:31:49 +0900

    refactor : 로그인 API 함수 수정

--- src/main/java/com/cheolhyeon/diary/DiaryApplication.java
+++ src/main/java/com/cheolhyeon/diary/DiaryApplication.java
@@ -3,9 +3,11 @@ package com.cheolhyeon.diary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableWebSecurity
 public class DiaryApplication {
 
     public static void main(String[] args) {
