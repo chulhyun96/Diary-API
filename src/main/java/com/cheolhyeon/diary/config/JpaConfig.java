669c7e97d7c07aa90185cde333bc443db61627bc
chulhyun96 <park82602@gmail.com>
2025-09-07 18:14:34 +0900

    프로젝트 초기화

--- src/main/java/com/cheolhyeon/diary/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cheolhyeon.diary.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-} 
