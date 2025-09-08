5091c5f8e8a4be471a54b28d5e984a2827913179
chulhyun96 <park82602@gmail.com>
2025-09-08 19:51:58 +0900

    refactor : 카카오API 서버 필요치 않는 필드 불러오기로 인한 JsonIgnoreProperties 애노테이션 추가

--- src/main/java/com/cheolhyeon/diary/auth/dto/response/KakaoUserInfoResponse.java
+++ src/main/java/com/cheolhyeon/diary/auth/dto/response/KakaoUserInfoResponse.java
@@ -1,22 +1,25 @@
 package com.cheolhyeon.diary.auth.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserInfoResponse {
     private Long id;
     private KakaoAccount kakao_account;
 
     @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class KakaoAccount {
         private Profile profile;
+
+
         @Getter
+        @JsonIgnoreProperties(ignoreUnknown = true)
         public static class Profile {
             private String nickname;
         }
