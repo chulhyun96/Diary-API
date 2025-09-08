ca9ec8a906ae6b058ec93017d56461bad57b5e59
chulhyun96 <park82602@gmail.com>
2025-09-08 19:51:10 +0900

    refactor : Servcie클래스 네이밍 변경

--- src/main/java/com/cheolhyeon/diary/auth/controller/KakaoTokenController.java
+++ src/main/java/com/cheolhyeon/diary/auth/controller/KakaoTokenController.java
@@ -1,7 +1,7 @@
 package com.cheolhyeon.diary.auth.controller;
 
 import com.cheolhyeon.diary.auth.dto.response.KakaoUserInfoResponse;
-import com.cheolhyeon.diary.auth.service.LoginService;
+import com.cheolhyeon.diary.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class KakaoTokenController {
-    private final LoginService loginService;
+    private final UserService userService;
 
     @GetMapping("/login/oauth2/code/kakao")
     public ResponseEntity<KakaoUserInfoResponse> getKakaoCode(String code) {
-        KakaoUserInfoResponse me = loginService.getKakaoLoginCode(code);
+        KakaoUserInfoResponse me = userService.processLogin(code);
         return ResponseEntity.ok(me);
     }
 
     @GetMapping("/login")
-    public ResponseEntity<Void> getLoginUrl() {
-        String url = loginService.getLoginUrl();
+    public ResponseEntity<Void> login() {
+        String url = userService.getLoginUrl();
         return ResponseEntity
                 .status(302)
                 .header("Location", url)
