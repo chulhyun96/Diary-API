889d701e6a53fa95a9294a0151ee601f4d295c04
chulhyun96 <park82602@gmail.com>
2025-09-09 19:27:48 +0900

    refactor : yml Configuration 객체화

--- src/main/java/com/cheolhyeon/diary/auth/component/KakaoApiComponent.java
+++ src/main/java/com/cheolhyeon/diary/auth/component/KakaoApiComponent.java
@@ -2,41 +2,40 @@ package com.cheolhyeon.diary.auth.component;
 
 import com.cheolhyeon.diary.auth.dto.request.KakaoTokenRequest;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @Getter
+@Setter
 @Component
+@ConfigurationProperties(prefix = "kakao.client")
 public class KakaoApiComponent {
-    @Value("${kakao.client.id}")
-    private String kakaoClientId;
-    @Value("${kakao.client.secret}")
-    private String kakaoClientSecret;
-    @Value("${kakao.client.redirect_url}")
+    private String id;
+    private String secret;
     private String redirectUrl;
-    @Value("${kakao.client.content_type}")
     private String contentType;
-    @Value("${kakao.client.kakao_auth_url}")
     private String kakaoAuthUrl;
-    @Value("${kakao.client.response_type}")
     private String responseType;
+
 
     public KakaoTokenRequest getKakaoTokenRequest(String code) {
         return KakaoTokenRequest.builder()
-                .client_id(kakaoClientId)
-                .client_secret(kakaoClientSecret)
+                .client_id(id)
+                .client_secret(secret)
                 .code(code)
                 .grant_type("authorization_code")
                 .redirect_uri(redirectUrl)
                 .build();
     }
+
     public String buildLoginUrl() {
         try {
             return kakaoAuthUrl +
-                    "?client_id=" + URLEncoder.encode(kakaoClientId, StandardCharsets.UTF_8) +
+                    "?client_id=" + URLEncoder.encode(id, StandardCharsets.UTF_8) +
                     "&redirect_uri=" + URLEncoder.encode(redirectUrl, StandardCharsets.UTF_8) +
                     "&response_type=" + responseType;
         } catch (Exception e) {
