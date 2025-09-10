f8d28683577a9f8c55f75b98f61f4ae3ce9330d1
chulhyun96 <park82602@gmail.com>
2025-09-10 20:50:51 +0900

    refactor : Jwt도 함께 반환될 수 있도록 리팩토링

--- src/main/java/com/cheolhyeon/diary/auth/service/UserService.java
+++ src/main/java/com/cheolhyeon/diary/auth/service/UserService.java
@@ -1,13 +1,15 @@
 package com.cheolhyeon.diary.auth.service;
 
-import com.cheolhyeon.diary.auth.component.KakaoApiComponent;
+import com.cheolhyeon.diary.app.feign.external.KakaoApiClient;
+import com.cheolhyeon.diary.app.feign.external.KakaoTokenClient;
+import com.cheolhyeon.diary.app.properties.KakaoApiProperties;
 import com.cheolhyeon.diary.auth.dto.request.KakaoTokenRequest;
 import com.cheolhyeon.diary.auth.dto.response.KakaoTokenResponse;
 import com.cheolhyeon.diary.auth.dto.response.KakaoUserInfoResponse;
-import com.cheolhyeon.diary.app.feign.external.KakaoApiClient;
-import com.cheolhyeon.diary.app.feign.external.KakaoTokenClient;
 import com.cheolhyeon.diary.auth.entity.User;
 import com.cheolhyeon.diary.auth.repository.UserRepository;
+import com.cheolhyeon.diary.token.TokenService;
+import com.cheolhyeon.diary.token.dto.response.JwtResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,15 +21,17 @@ public class UserService {
     private final KakaoTokenClient kakaoTokenClient;
     private final KakaoApiClient kakaoApiClient;
     private final UserRepository userRepository;
-    private final KakaoApiComponent kakaoApiComponent;
+
+    private final KakaoApiProperties kakaoApiProperties;
+    private final TokenService tokenService;
 
     @Transactional
     public KakaoUserInfoResponse processLogin(String code) {
-        KakaoTokenRequest kakaoTokenRequest = kakaoApiComponent.getKakaoTokenRequest(code);
-        KakaoTokenResponse kakaoTokenResponse = kakaoTokenClient.getToken(kakaoApiComponent.getContentType(), kakaoTokenRequest);
+        KakaoTokenRequest kakaoTokenRequest = kakaoApiProperties.getKakaoTokenRequest(code);
+        KakaoTokenResponse kakaoTokenResponse = kakaoTokenClient.getToken(kakaoApiProperties.getContentType(), kakaoTokenRequest);
         KakaoUserInfoResponse client = kakaoApiClient.getMe("Bearer " + kakaoTokenResponse.getAccess_token());
 
-        userRepository.findById(client.getId())
+        User user = userRepository.findById(client.getId())
                 .map(existedUser -> {
                     existedUser.updateLastLoginTime();
                     return userRepository.save(existedUser);
@@ -36,11 +40,13 @@ public class UserService {
                     User newUser = User.createUser(client);
                     return userRepository.save(newUser);
                 });
+        JwtResponse jwtResponse = tokenService.createJwt(user.getKakaoId());
+        client.addResponse(jwtResponse);
         return client;
     }
 
     public String getLoginUrl() {
-        return kakaoApiComponent.buildLoginUrl();
+        return kakaoApiProperties.buildLoginUrl();
     }
 
 }
