c7e57d0cd409750e17e1df9c4162149af8bc739a
chulhyun96 <park82602@gmail.com>
2025-09-16 19:53:07 +0900

    feat : 일관된 Excpetion을 처리하기 위한 중앙집중관리방식

--- src/main/java/com/cheolhyeon/diary/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cheolhyeon/diary/app/exception/GlobalExceptionHandler.java
@@ -1,8 +1,22 @@
 package com.cheolhyeon.diary.app.exception;
 
+import com.cheolhyeon.diary.app.exception.dto.ErrorResponse;
+import com.cheolhyeon.diary.app.exception.user.UserException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(UserException.class)
+    public ErrorResponse handleUserException(ErrorStatus errorStatus) {
+        log.error("UserException ErrorCode : {},  ErrorMessage : {}, ErrorDesc : {}",
+                errorStatus.getErrorCode(),
+                errorStatus.getErrorMessage(),
+                errorStatus.getErrorDescription()
+        );
+        return ErrorResponse.create(errorStatus);
+    }
 }
