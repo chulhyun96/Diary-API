943f2b9ecce780653f7e54e25d1ff79a273ecee3
chulhyun96 <park82602@gmail.com>
2025-09-16 19:52:00 +0900

    feat : Error 메시지를 일관적으로 반환하기 위해서 Response 객체 생성

---
+++ src/main/java/com/cheolhyeon/diary/app/exception/dto/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.cheolhyeon.diary.app.exception.dto;
+
+import com.cheolhyeon.diary.app.exception.ErrorStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int errorCode;
+    private final String errorMessage;
+    private final String errorDescription;
+
+
+    public static ErrorResponse create(ErrorStatus errorStatus) {
+        return new ErrorResponse(
+                errorStatus.getErrorCode(),
+                errorStatus.getErrorMessage(),
+                errorStatus.getErrorDescription());
+    }
+}
