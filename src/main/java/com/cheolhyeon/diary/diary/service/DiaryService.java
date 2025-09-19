7aff13f9d081dd5aab4df0257f492e225ad59a3b
chulhyun96 <park82602@gmail.com>
2025-09-19 11:44:34 +0900

    refactor : 보상 트랜잭션 로직을 EventListener로 처리
    - 기존에는 TransactionSynchronizationManager의 afterCompletion으로 Rollback시 이미지 파일들을 롤백처리할 수 있도록 구현했으나, 코드의 가독성과 복잡성을 증가시켜 publshEvent로 변경

--- src/main/java/com/cheolhyeon/diary/diary/service/DiaryService.java
+++ src/main/java/com/cheolhyeon/diary/diary/service/DiaryService.java
@@ -2,26 +2,27 @@ package com.cheolhyeon.diary.diary.service;
 
 import com.cheolhyeon.diary.app.exception.user.UserErrorStatus;
 import com.cheolhyeon.diary.app.exception.user.UserException;
+import com.cheolhyeon.diary.app.util.UlidGenerator;
 import com.cheolhyeon.diary.auth.entity.User;
 import com.cheolhyeon.diary.auth.repository.UserRepository;
+import com.cheolhyeon.diary.diary.dto.S3RollbackCleanup;
 import com.cheolhyeon.diary.diary.dto.reqeust.DiaryRequest;
 import com.cheolhyeon.diary.diary.dto.response.DiaryResponse;
 import com.cheolhyeon.diary.diary.entity.Diaries;
 import com.cheolhyeon.diary.diary.repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class DiaryService {
+    private final ApplicationEventPublisher eventPublisher;
     private final DiaryRepository diaryRepository;
     private final UserRepository userRepository;
     private final S3Service s3Service;
@@ -32,25 +33,11 @@ public class DiaryService {
         User writer = userRepository.findById(kakaoId)
                 .orElseThrow(() -> new UserException(UserErrorStatus.NOT_FOUND));
 
-        List<String> keys = new ArrayList<>();
-        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
-            @Override
-            public void afterCompletion(int status) {
-                if (status == TransactionSynchronization.STATUS_ROLLED_BACK) {
-                    keys.forEach(k -> {
-                        try {
-                            s3Service.delete(k);
-                        } catch (Exception ignore) {
-                            // 옵션: 추후 재시도용
-                        }
-                    });
-                }
-            }
-        });
+        List<String> keys = s3Service.upload(writer.getKakaoId(), images);
+        eventPublisher.publishEvent(new S3RollbackCleanup(keys));
 
-        List<String> upload = s3Service.upload(writer.getKakaoId(), images);
-        keys.addAll(upload);
-        Diaries entity = DiaryRequest.toEntity(kakaoId, writer.getDisplayName(), keys, request);
+        byte[] ulid = UlidGenerator.generatorUlid();
+        Diaries entity = DiaryRequest.toEntity(ulid, kakaoId, writer.getDisplayName(), keys, request);
         return DiaryResponse.toResponse(diaryRepository.save(entity));
     }
 }
