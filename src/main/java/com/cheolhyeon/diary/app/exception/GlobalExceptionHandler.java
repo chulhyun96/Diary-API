4042821a737db767226b263ce4fe2993a2636d3e
chulhyun96 <park82602@gmail.com>
2025-09-19 12:17:57 +0900

    feat : 사용자 정의 S3Exception 클래스

--- src/main/java/com/cheolhyeon/diary/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cheolhyeon/diary/app/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.cheolhyeon.diary.app.exception;
 
 import com.cheolhyeon.diary.app.exception.diary.DiaryException;
 import com.cheolhyeon.diary.app.exception.dto.ErrorResponse;
+import com.cheolhyeon.diary.app.exception.s3.S3Exception;
 import com.cheolhyeon.diary.app.exception.user.UserException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserException.class)
     public ErrorResponse handleUserException(ErrorStatus errorStatus) {
-        log.error("UserException ERRORCODE : {},  ERRORMESSGE : {}, ERRORDESCRIPTION : {}",
+        log.error("UserException ERROR CODE : {},  ERROR MESSAGE : {}, ERROR DESCRIPTION : {}",
                 errorStatus.getErrorCode(),
                 errorStatus.getErrorMessage(),
                 errorStatus.getErrorDescription()
@@ -23,7 +24,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DiaryException.class)
     public ErrorResponse handleDiaryException(ErrorStatus errorStatus) {
-        log.error("DiaryException ERRORCODE : {}, ERRORMESSGE : {}, ERRORDESCRIPTION : {}",
+        log.error("DiaryException ERROR CODE : {}, ERROR MESSAGE : {}, ERROR DESCRIPTION : {}",
+                errorStatus.getErrorCode(),
+                errorStatus.getErrorMessage(),
+                errorStatus.getErrorDescription()
+        );
+        return ErrorResponse.of(errorStatus);
+    }
+    @ExceptionHandler(S3Exception.class)
+    public ErrorResponse handleS3Exception(ErrorStatus errorStatus) {
+        log.error("S3Exception ERROR CODE : {}, ERROR MESSAGE : {}, ERROR DESCRIPTION : {}",
                 errorStatus.getErrorCode(),
                 errorStatus.getErrorMessage(),
                 errorStatus.getErrorDescription()
