3eafed4725d3d692f1e34dd527565b3cfb2c54de
chulhyun96 <park82602@gmail.com>
2025-09-24 20:05:47 +0900

    refactor : S3ErrorStatus 예외 처리 개선

--- src/main/java/com/cheolhyeon/diary/app/exception/s3/S3ErrorStatus.java
+++ src/main/java/com/cheolhyeon/diary/app/exception/s3/S3ErrorStatus.java
@@ -3,7 +3,6 @@ package com.cheolhyeon.diary.app.exception.s3;
 import com.cheolhyeon.diary.app.exception.ErrorStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
@@ -16,11 +15,17 @@ public enum S3ErrorStatus implements ErrorStatus , Serializable {
     FAILED_UPLOAD_IMAGE(
             HttpStatus.BAD_REQUEST.value(),
             HttpStatus.BAD_REQUEST.getReasonPhrase(),
-            "이미지 저장 실패"
-    ), FAILED_LOAD_IMAGE(
+            "내부 서버 오류로 인해 이미지 저장이 실패했습니다."
+    ),
+    FAILED_LOAD_IMAGE(
             HttpStatus.INTERNAL_SERVER_ERROR.value(),
             HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-            "내부 서버 오류로 인해 이미지 불러오기가 실패했습니다." );
+            "내부 서버 오류로 인해 이미지 불러오기가 실패했습니다." ),
+    FAILED_DELETE_IMAGE(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+            "내부 서버 오류로 이미지 삭제가 실패했습니다." )
+    ;
     private final int errorCode;
     private final String errorMessage;
     private final String errorDescription;
